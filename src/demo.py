44e6653bf870acf1442e7d0b0aead6d0937b187d
Michael Mayo <michaelmayo704@gmail.com>
2025-07-20 16:50:16 +1200

    added error handling to get data and updated demo program

--- src/demo.py
+++ src/demo.py
@@ -3,12 +3,14 @@ from backtesting.lib import crossover
 
 from backtesting.test import SMA, GOOG
 
+from get_data import get_data
+
 
 class SmaCross(Strategy):
     def init(self):
         price = self.data.Close
-        self.ma1 = self.I(SMA, price, 10)
-        self.ma2 = self.I(SMA, price, 20)
+        self.ma1 = self.I(SMA, price, 20)
+        self.ma2 = self.I(SMA, price, 200)
 
     def next(self):
         if crossover(self.ma1, self.ma2):
@@ -17,7 +19,9 @@ class SmaCross(Strategy):
             self.sell()
 
 
-bt = Backtest(GOOG, SmaCross, commission=.002,
+bt = Backtest(get_data("TQQQ",start_date="2010-01-01"),
+              SmaCross,
+              commission=.002,
               exclusive_orders=True)
 stats = bt.run()
 print(stats)
--- src/get_data.py
+++ src/get_data.py
@@ -20,15 +20,20 @@ def get_data(ticker: str,
                          start=start_date, end=end_date,
                          auto_adjust=True).sort_index()
         df.columns = map(lambda t: t[0], list(df.columns))
-        df.to_csv(path)
+        if df is not None and len(df)>0:
+            df.to_csv(path)
         time.sleep(1)
+    df.index = pd.to_datetime(df.index)
     return df
 
 if __name__=="__main__":
     if len(sys.argv)<2:
-        print("usage: python get_data.py <ticker>")
+        print("usage: python get_data.py <ticker> [<YYYY-MM-DD> <YYYY-MM-DD>]")
         exit()
-    df=get_data(sys.argv[1])
+    if len(sys.argv)==2:
+        df=get_data(sys.argv[1])
+    else:
+        df=get_data(sys.argv[1],start_date=sys.argv[2],end_date=sys.argv[3])
     if df is None or len(df)==0:
         print("get_data.py failed")
     else:
