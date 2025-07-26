5131673a57e70ce57664950c62e0ae7e1f09e5e3
Michael Mayo <michaelmayo704@gmail.com>
2025-07-26 17:30:46 +1200

    update

--- src/simple.py
+++ src/simple.py
@@ -2,31 +2,28 @@ import numpy as np
 import pandas as pd
 
 from backtesting import Backtest, Strategy
-from backtesting.lib import crossover
-from backtesting.test import SMA
 
 from get_data import get_data
 
 # define strategy
 class Simple(Strategy):
 
-    lookbacks=[1,3,8,21,55,144]
-    coefs=[0]*len(lookbacks)
+    s=200
+    q_in,q_out=0.5,0.4
 
     # constructor does nothing for now
     def init(self):
-        pass
+        assert self.q_in>self.q_out, "q_in must be > than q_out"
 
     # strategy actions for next day
     def next(self):
-        n=len(self.coefs)
-        if len(self.data.Close)<np.max(self.lookbacks):
+        if len(self.data.Close)<self.s:
             return
-        prices=list(map(lambda i: self.data.Close[-1*i],self.lookbacks))
-        f=np.multiply(prices,self.coefs).sum()
-        if not self.position and f>=0:
-            self.buy(sl=self.data.Close[-1]*0.9)
-        elif self.position and f<0:
+        price_in=np.quantile(self.data.Close[(-1*self.s):],self.q_in)
+        price_out=np.quantile(self.data.Close[(-1*self.s):],self.q_out)
+        if not self.position and self.data.Close[-1]>=price_in:
+            self.buy(sl=self.data.Close[-1]*0.95)
+        elif self.position and self.data.Close[-1]<price_out:
             self.position.close()
 
 
@@ -35,41 +32,21 @@ class Simple(Strategy):
 def hatch_commission(order_size,_):
     return 3+max(0,order_size-300)*0.01
 
-# optimise
-def optimise(data,n):
-    # define a set of param combinations
-    xx=[ (np.random.random(size=len(Simple.coefs))*4-2).tolist() for _ in range(n) ]
-    # run multiple simulations on the same data with different coefs
-    results = []
-    for x in xx:
-        Simple.coefs=x
-        bt=Backtest(data,Simple,cash=1000,
+# run
+def run(data):
+        bt=Backtest(data,Simple,cash=10000,
                     commission=hatch_commission,
                     exclusive_orders=True,
                     trade_on_close=False)
         stats=bt.run()
-        results.append([stats["CAGR [%]"],*x])
-    results=pd.DataFrame(results)
-    results.columns=["CAGR", *[f"c{i}" for i in range(len(results.columns)-1)]]
-    results=results.sort_values("CAGR",ascending=False).reset_index(drop=True)
-    # display results
-    print(results)
-    # validate the best set of coefs
-    best=results.iloc[0,1:].values
-    Simple.coefs=best
-    print(best)
-    bt=Backtest(data,Simple,cash=10000,
-                commission=hatch_commission,
-                exclusive_orders=True,
-                trade_on_close=False)
-    stats=bt.run()
-    print(stats)
-    print(stats._trades)
+        print(stats)
+        print(stats._trades)
+        bt.plot()
 
 
 
 
 
-optimise(get_data("TQQQ",start_date="2019-01-01"),100)
+run(get_data("TQQQ",start_date="2019-01-01"))
 
 
